d738bfc4a6ef4b5851213cfe4f923e753e6d59fb
Nathalie Echer <163636824+Nech691@users.noreply.github.com>
2024-05-02 16:34:19 +1200

    Works - need to make pretty

--- sketch.js
+++ sketch.js
@@ -1,41 +1,41 @@
-// declaring objects, variables, strings and arrays 
-let ballSystem1 = [];
-let ballSystem2 = [];
-let ballSystem3 = [];
+// declaring what needs to be declared (objects, variables, strings and arrays)
+let heartLine1 = [];
+let heartLine2 = [];
+let heartLine3 = [];
 
 function setup() {
   createCanvas(400, 400);
   for (x = 0; x < 3; x++){
     let rx = random(15, width - 15); // -> Begining x position 
-    ballSystem1[x] = new Ball(rx, 100, 40); // -> Set up first line 
-    ballSystem2[x] = new Ball(rx, 200, 30); // -> Set up second line
-    ballSystem3[x] = new Ball(rx, 300, 10); // Set up third line 
+    heartLine1[x] = new Heart(rx, 100, 40); // -> Set up first line 
+    heartLine2[x] = new Heart(rx, 200, 30); // -> Set up second line
+    heartLine3[x] = new Heart(rx, 300, 10); // Set up third line 
   }
 }
 
 
 function draw() {
   background(220);
-  for (x = 0; x < ballSystem1.length; x++){ // -> 1st line's show, move and checkEdges
-    ballSystem1[x].move();
-    ballSystem1[x].show();
-    ballSystem1[x].checkEdges();
+  for (x = 0; x < heartLine1.length; x++){ // -> 1st line's show, move and checkEdges
+    heartLine1[x].move();
+    heartLine1[x].show();
+    heartLine1[x].checkEdges();
   }
 
-  for (x = 0; x < ballSystem2.length; x++){ // -> 2nd line's show, move and checkEdges
-    ballSystem2[x].move();
-    ballSystem2[x].show();
-    ballSystem2[x].checkEdges();
+  for (x = 0; x < heartLine2.length; x++){ // -> 2nd line's show, move and checkEdges
+    heartLine2[x].move();
+    heartLine2[x].show();
+    heartLine2[x].checkEdges();
   }
 
-  for (x = 0; x < ballSystem3.length; x++){ // -> 3nd line's show, move and checkEdges
-    ballSystem3[x].move();
-    ballSystem3[x].show();
-    ballSystem3[x].checkEdges();
+  for (x = 0; x < heartLine3.length; x++){ // -> 3nd line's show, move and checkEdges
+    heartLine3[x].move();
+    heartLine3[x].show();
+    heartLine3[x].checkEdges();
   }
 }
 
-class Ball { // Creating a "Ball" class
+class Heart { // Creating a "Heart" class
   constructor(x, y, r){ // Setting up blue print 
     this.x = x; // Refering to current object (this instant in use)
     this.y = y; //     ""            ""             ""
@@ -48,22 +48,35 @@ class Ball { // Creating a "Ball" class
   }
 
   show(){
-    fill(255, 0, 0, 160); // ball colour 
-    noStroke(); // ball w/o stroke 
-    ellipse(this.x, this.y, this.r, this.r); // Make ball   
+    fill(255, 0, 0, 160); // Heart colour 
+    noStroke(); // Heart w/o stroke 
+    heartShape(this.x, this.y, this.r); // Draw heart shape
   }
 
-  checkEdges(){ // function to check if balls have reached edge 
-    if (this.x < 15){ //if ball is more than half way covered on the left 
+  checkEdges(){ // function to check if heart have reached edge 
+    if (this.x < 15){ //if heart is more than half way covered on the left 
       this.x = 15; // don't let it get past that 
-    } else if (this.x > width - 15){ //if ball is more than half way covered on the right 
+    } else if (this.x > width - 15){ //if heart is more than half way covered on the right 
       this.x = width - 15 // don't let it get past that 
     }
 
-    if (this.y < 15){ //if ball is more than half way covered on the tp 
+    if (this.y < 15){ //if heart is more than half way covered on the tp 
       this.y = 15; // don't let it get past that 
-    } else if (this.y > width - 15){ //if ball is more than half way covered on the bottom 
+    } else if (this.y > width - 15){ //if heart is more than half way covered on the bottom 
       this.y = width - 15 // don't let it get past that 
     }
   }
+}
+
+// -> Function for drawing heart shapes 
+// - Made with the help of ChatGPT
+function heartShape(x, y, r) {
+  beginShape(); // - Initiate shape making
+  // - Make a series of Bezier curves to make up a heart 
+  vertex(x, y + r / 2);
+  bezierVertex(x, y - r / 2, x + r, y - r / 2, x + r, y + r / 2);
+  bezierVertex(x + r, y + r, x, y + 2 * r, x, y + 2 * r);
+  bezierVertex(x, y + 2 * r, x - r, y + r, x - r, y + r / 2);
+  bezierVertex(x - r, y - r / 2, x, y - r / 2, x, y + r / 2);
+  endShape(CLOSE); // - End shape making 
 }
